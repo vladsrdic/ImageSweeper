7dac84cac7bc97ac7a59d1e24e3d605b481d5b96
Vladimir Srdic <vlad.srdic@outlook.com>
2023-09-25 11:48:01 -0600

    Add next config for being able to fetch images from reddit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+// https://i.redd.it/here-see-my-majestic-cat-v0-2kircypylreb1.jpg?s=41db67ac90938a3182f393de512482571862cf3f
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'i.redd.it',
+                port: '',
+                pathname: '/**',
+            }
+        ]
+    }
+}
 
 module.exports = nextConfig
